8477c8d3a4fb53dbe72439612b1a7cac8b18d756
checkaf <ashrifqs@icloud.com>
2026-09-06 17:18:02 +0530

    patient

---
+++ Patient.java
@@ -0,0 +1,55 @@
+public class Patient {
+
+    private int patientId;
+    private String patientName;
+    private int age;
+    private String contactNumber;
+    private String medicalCondition;
+    private VisitHistory visitHistory;
+
+    public Patient(int patientId, String patientName, int age,
+                   String contactNumber, String medicalCondition) {
+
+        this.patientId = patientId;
+        this.patientName = patientName;
+        this.age = age;
+        this.contactNumber = contactNumber;
+        this.medicalCondition = medicalCondition;
+        this.visitHistory = new VisitHistory();
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getMedicalCondition() {
+        return medicalCondition;
+    }
+
+    public VisitHistory getVisitHistory() {
+        return visitHistory;
+    }
+
+    public void displayPatient() {
+
+        System.out.println();
+        System.out.println("Patient ID       : " + patientId);
+        System.out.println("Patient Name     : " + patientName);
+        System.out.println("Age              : " + age);
+        System.out.println("Contact Number   : " + contactNumber);
+        System.out.println("Medical Condition: " + medicalCondition);
+        System.out.println();
+    }
+}
